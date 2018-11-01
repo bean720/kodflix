117c5eb386f848da110fb8d34bef8f195fd28c2a
Abdul Amin <Macbook 2@MacBook-Pro.local>
2018-11-01 13:19:00 +0000

    Created 3 columns 2 rows of text as flexgrid #3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import evil from './evil.jpg';
+import evil from './img/evil.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,34 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-body">
-          <img src={evil} className="App-evil" alt="evil" />
+        
+          <div className="wrap-img">
+            <img src={evil} className="App-evil" alt="evil" />
+          </div>
+          <div className="flex-grid">
+            <div className="col">
+              <h2 className="Text1">One</h2>
+            </div>
+            <div className="col">
+              <h2 className="Text2">Two</h2>
+            </div>
+            <div className="col">
+              <h2 className="Text3">Three</h2>
+            </div>
+          </div>  
+          <div className="flex-grid">
+            <div className="col">
+              <h2 className="Text4">Four</h2>
+            </div>
+            <div className="col">
+              <h2 className="Text5">Five</h2>
+            </div>
+            <div className="col">
+              <h2 className="Text6">Six</h2>
+            </div>
+          </div> 
+
+          
         </header>
       </div>
     );
