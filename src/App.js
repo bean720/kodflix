ac976a733eb35c8fba0a16ef2475644b8d4a5156
Abdul Amin <Macbook 2@MacBook-Pro.local>
2018-11-01 11:15:54 +0000

    Added evil jpg and set width #1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import evil from './evil.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to KodflixTest</h1>
+        <header className="App-body">
+          <img src={evil} className="App-evil" alt="evil" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
