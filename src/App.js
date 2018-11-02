b6995974e4d1d35200798049ffdd95bc0b4fd51b
Abdul Amin <Macbook 2@MacBook-Pro.local>
2018-11-02 12:06:20 +0000

    css flex direction column&media type for imgs to appear in one column #4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import one from './img/one.jpg';
-import wire from './img/wire.jpg';
+import wire from './img/wire.jpeg';
 import avatar from './img/avatar.jpg';
-import game from './img/game.jpeg';
+import game from './img/game.jpg';
 import monkey from './img/monkey.jpg';
-import evil from './img/evil.jpg';
+import death from './img/death.jpg';
 import './App.css';
 
 class App extends Component {
@@ -13,47 +13,33 @@ class App extends Component {
       <div className="App">
         <header className="App-body">
         
-          <div className="wrap-img">
-            <img src={evil} className="App-evil" alt="evil" />
-          </div>
+         
           <div className="flex-grid">
             <div className="col">
-              <div className="wrap-img">
-                <img src={one} className="App-one" alt="one" />
-              </div>
+              <img src={one} className="App-image" alt="one" />
               <h2 className="Text">One piece</h2>
             </div>
             <div className="col">
-              <div className="wrap-img">
-                <img src={wire} className="App-wire" alt="wire" />
-              </div>
+              <img src={wire} className="App-image" alt="wire" />
               <h2 className="Text">The Wire</h2>
             </div>
             <div className="col">
-              <div className="wrap-img">
-                <img src={avatar} className="App-avatar" alt="avatar" />
-              </div>
+              <img src={avatar} className="App-image" alt="avatar" />
               <h2 className="Text">Avatar</h2>
             </div>
           </div>  
           <div className="flex-grid">
             <div className="col">
-              <div className="wrap-img">
-                <img src={game} className="App-game" alt="game" />
-              </div>
+              <img src={game} className="App-image" alt="game" />
               <h2 className="Text">Game of Thrones</h2>
             </div>
             <div className="col">
-              <div className="wrap-img">
-                <img src={monkey} className="App-monkey" alt="monkey" />
-              </div>
+              <img src={monkey} className="App-image" alt="monkey" />
               <h2 className="Text">Monkey</h2>
             </div>
             <div className="col">
-              <div className="wrap-img">
-                <img src={evil} className="App-evil" alt="evil" />
-              </div>
-              <h2 className="Text">See No Evil</h2>
+              <img src={death} className="App-image" alt="death" />
+              <h2 className="Text">Death Note</h2>
             </div>
           </div> 
 
